da5f29bdbe05777764786e50287f06522c168a1a
Lucian Green <luciangreen@lucianacademy.com>
2019-07-10 11:19:47 +1000

    Update mindreadtestmusiccomposer-unusual-4.pl

--- mindreadtestmusiccomposer-unusual-4.pl
+++ mindreadtestmusiccomposer-unusual-4.pl
@@ -2,5 +2,5 @@
 :- include('1451-4').
 :- include('1564-4').
 :- include('1645-4').
-%%:- include('popclassical-4').
-%%:- include('classical-4').
+:- include('popclassical-4').
+:- include('classical-4').
