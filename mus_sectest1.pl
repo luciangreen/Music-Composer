a6d31085bb8658cb8c4293fe0a1cad128d3712f9
Lucian Green <luciangreen@lucianacademy.com>
2024-05-12 11:01:38 +1000

    Fixed mus_sectest1.pl

--- mus_sectest1.pl
+++ mus_sectest1.pl
@@ -20,12 +20,15 @@ sectest01(N) :-
 
 sectest11(0) :- !.
 sectest11(N1) :-
-	catch(
-	((sectest0(_Form1,_Lyrics,_Melody,_Harmony,_MelodyParts,
-	_HarmonyParts,_Vocalstubinstrument,_Song1)->true;sectest11(N1)),
-	writeln([song,N1,done]),
+	(catch(
+	sectest0(_Form1,_Lyrics,_Melody,_Harmony,_MelodyParts,
+	_HarmonyParts,_Vocalstubinstrument,_Song1),
+	%(random(X),X1 is ceiling(2*X),X1 =1),
+	_,
+	false)->
+	(writeln([song,N1,done]),
 	N2 is N1-1,
-	sectest11(N2)),
-   _,
+	sectest11(N2)
+	);
 	sectest11(N1)
 ).
